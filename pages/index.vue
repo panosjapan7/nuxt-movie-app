578e1d75ab68c802f33f6869921b5599e62fa57a
panosjapan7 <panosjapan@gmail.com>
2022-07-07 13:37:12 +0200

    Search Feature

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,56 @@
     <!-- Hero -->
     <Hero />
 
+    <!-- Search -->
+    <div class="container search">
+      <input 
+        @keyup.enter="$fetch" 
+        type="text" 
+        placeholder="Search" 
+        v-model.lazy="searchInput" 
+      />
+      <button @click="clearSearch" v-show="searchInput !==''" class="button">Clear Search</button>
+    </div>
+
     <!-- Movies -->
     <div class="container movies">
-      <div id="movies-grid" class="movies-grid">
+      
+      <!-- Search Movies -->
+      <div v-if="searchInput !== ''" id="movies-grid" class="movies-grid">
+        <div v-for="(movie, index) in searchedMovies" :key="index" class="movie">
+          
+          <div class="movie-img">
+            <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt=""/>
+            <p class="review">{{ movie.vote_average }}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          
+          <div class="info">
+            <p class="title">{{ movie.title.slice(0, 25) }} <span v-if="movie.title.length > 25">...</span></p>
+
+            <p class="release">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleString("en-us", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })
+              }}
+            </p>
+            <NuxtLink 
+              class="button button-light" 
+              :to="{name: 'movies-movieid', params: { movieid: movie.id }}"
+            >
+              Get More Info
+            </NuxtLink>
+          </div>
+        
+        </div>
+      </div>
+
+      <!-- Now Streaming -->
+      <div v-else id="movies-grid" class="movies-grid">
         <div v-for="(movie, index) in movies" :key="index" class="movie">
           
           <div class="movie-img">
@@ -47,23 +94,45 @@ export default {
   data() {
     return {
       movies: [],
+      searchedMovies: [],
+      searchInput: "",
     }
   },
 
   async fetch() {
-    await this.getMovies()
+
+    if (this.searchInput === "") {
+      await this.getMovies()
+      return
+    }
+
+    await this.searchMovies()
   },
 
   methods: {
     async getMovies() {
       const data = axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=238cb64db6302e7587f0099a49af4b3d&language=en-US$page=1")
       const result = await data
-      result.data.results.forEach(movie => {
+      result.data.results.forEach((movie) => {
         this.movies.push(movie)
       })
-      console.log(this.movies)
+    },
+
+    async searchMovies () {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=238cb64db6302e7587f0099a49af4b3d&language=en-US&page=1&query=${this.searchInput}`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.searchedMovies.push(movie)
+      })
+    },
+
+    clearSearch () {
+      this.searchInput = ""
+      this.searchedMovies = []
     }
-  }
+  },
 }
 </script>
 
